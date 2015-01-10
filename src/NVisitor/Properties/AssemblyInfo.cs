f146fa90d85acc1c82eda59ee8a6bee1e8cd5999
Jerome <jeromerg@gmx.net>
2015-01-10 20:18:12 +0100

    changed assembly description

--- src/NVisitor/Properties/AssemblyInfo.cs
+++ src/NVisitor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NVisitor")]
-[assembly: AssemblyDescription("Main and unique productive assembly for the NVisitor framework")]
+[assembly: AssemblyDescription("Lightweight Framework to develop modular extendable visitors")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 
