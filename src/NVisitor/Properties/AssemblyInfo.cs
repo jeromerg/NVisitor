6aecfba61f6484b96305aa9325b6a9f7d0fdec7d
Jerome <jeromerg@gmx.net>
2015-01-10 20:15:24 +0100

    added nuget config for packaging the library, downgraded .net framework to 4.0 client profile

--- src/NVisitor/Properties/AssemblyInfo.cs
+++ src/NVisitor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NVisitor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Main and unique productive assembly for the NVisitor framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 
