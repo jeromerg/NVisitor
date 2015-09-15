f67926117ed250e9604e6317f9e6c3f843322a75
Jerome <jeromerg@gmx.net>
2015-09-15 19:57:52 +0200

    removed useless NGitVersion projects

--- src/Version/src/TestProject/CSharp/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// LOCAL
-[assembly: AssemblyTitle          ("TODO Title")]
-[assembly: AssemblyDescription    ("TODO Description")]
-[assembly: ComVisible             (false)]
-[assembly: Guid                   ("a5a4636c-1a18-4a67-aa7e-e2a10f44dd98")]
--- src/Version/src/TestProject/CppNative/CppNative.cpp
+++
@@ -1,6 +0,0 @@
-// CppNative.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
